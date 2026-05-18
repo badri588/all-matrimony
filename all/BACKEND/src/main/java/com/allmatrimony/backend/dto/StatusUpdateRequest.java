64a5d538a3728905f0b9ac402c66813e3a61d0da
Pranayini <pranay12ibliss@gmail.com>
2026-05-18 11:59:50 +0530

    Added backend source code without secrets

---
+++ all/BACKEND/src/main/java/com/allmatrimony/backend/dto/StatusUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.allmatrimony.backend.dto;
+
+public class StatusUpdateRequest {
+    private String adminMessage;
+
+    public String getAdminMessage() {
+        return adminMessage;
+    }
+
+    public void setAdminMessage(String adminMessage) {
+        this.adminMessage = adminMessage;
+    }
+}
